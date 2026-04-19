a23b0a83b10512986b71b535b4c8ee25dfc6ce3f
AMAN PANDEY <amanpandey12999@gmail.com>
2026-04-19 15:46:36 +0530

    Enhance DelhiMetro class with route management
    Added route details structure and methods for routes.

--- metro.h
+++ metro.h
@@ -1,18 +1,30 @@
 #ifndef METRO_H
 #define METRO_H
-#include<string>
-#include<map>
+
+#include <string>
+#include <map>
+#include <vector>
+
 using namespace std;
-class DelhiMetro{
-    int V;
-    map<int,string> stationName;
 
-   public: 
-   DelhiMetro(int v);
-void addStation(int id,string name);
-void displayStations(); 
-
+// Structure to store route details
+struct Edge {
+    int to;
+    int time;
+    int fare;
 };
 
+class DelhiMetro {
+    int V; // Number of stations
+    map<int, string> stationNames;
+    vector<vector<Edge>> adj;
+
+public:
+    DelhiMetro(int v);
+    void addStation(int id, string name);
+    void addRoute(int u, int v, int time, int fare); 
+    void displayStations(); 
+    void findShortestRoute(int start, int end);
+};
 
 #endif
